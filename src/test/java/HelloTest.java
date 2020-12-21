7637be8421553d5ea19bc2a3b435c445969b685c
git config --global user.name <yonashaile12@gmail.com>
2020-12-21 13:41:53 -0600

    added simple Junit static import

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -1,10 +1,16 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
+import static org.junit.jupiter.api.Assertions.*;
 public class HelloTest {
 
     @Test
     public void test(){
         Assertions.assertEquals(4, 3+1);
+    }
+
+    @Test
+    public void test2(){
+        // assert 4 time 3 is 12
+        assertEquals(12, 3*4);
     }
 }
