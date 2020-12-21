99dba257586eb43f90ee29f23f6c156992176154
git config --global user.name <yonashaile12@gmail.com>
2020-12-21 13:49:52 -0600

    added simple Junit annotations

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -1,16 +1,37 @@
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 public class HelloTest {
 
+
+    @BeforeAll
+    public static void setUp(){
+        System.out.println("@BeforeAll is running");
+    }
+
+    @AfterAll
+    public static void tearDown(){
+        System.out.println("AfterAll is running");
+    }
+    @BeforeEach
+    public void beforeEach(){
+        System.out.println("@BeforeEach is running");
+    }
+
+    @AfterEach
+    public void afterEach(){
+        System.out.println("@AfterEach is running");
+    }
     @Test
     public void test(){
         Assertions.assertEquals(4, 3+1);
+        System.out.println("Test1 is running");
     }
 
     @Test
     public void test2(){
         // assert 4 time 3 is 12
         assertEquals(12, 3*4);
+        System.out.println("Test2 is running");
     }
 }
